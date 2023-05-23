c590e760ee2c7a3c969328dd801003ca88372d79
Duy Nguyen <nguyenvanduy1816@gmail.com>
2023-05-23 22:14:53 +0700

    Done the task

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from "./components/Button/Button";
+import RegisterModal from "./components/Modal/RegisterModal";
+import useRegisterModal from "./hooks/useRegisterModal";
 
 function App() {
+  const registerModal = useRegisterModal();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="h-screen flex flex-col items-center justify-center">
+        <div className="bg-gray-200 p-6 rounded-lg">
+          <Button
+            outline
+            label="Open Modal Example"
+            onClick={registerModal.onOpen}
+          />
+        </div>
+      </div>
+      <RegisterModal />
     </div>
   );
 }
